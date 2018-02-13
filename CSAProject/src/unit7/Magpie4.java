76f64a7fdbb3465d5be5c6d5e949747d81768767
Aidan Tan <TanA5915@sduhsd.lan>
2018-02-13 11:04:59 -0800

    f gdgd

--- CSAProject/src/unit7/Magpie4.java
+++ CSAProject/src/unit7/Magpie4.java
@@ -52,7 +52,7 @@ public class Magpie4
 		// Responses which require transformations
 		else if (findKeyword(statement, "I want to", 0) >= 0)
 		{
-			response = transformIWantToStatement(statement);
+			response = transformIWantStatement(statement);
 		}
 
 		else
@@ -80,22 +80,39 @@ public class Magpie4
 	 * @param statement the user statement, assumed to contain "I want to"
 	 * @return the transformed statement
 	 */
-	private String transformIWantToStatement(String statement)
+	private String transformIWantStatement(String statement)
 	{
 		//  Remove the final period, if there is one
 		statement = statement.trim();
 		String lastChar = statement.substring(statement
 				.length() - 1);
-		if (lastChar.equals("."))
+		if (lastChar.equals(".")|| lastChar.equals("!"))
 		{
 			statement = statement.substring(0, statement
 					.length() - 1);
 		}
 		int psn = findKeyword (statement, "I want to", 0);
-		String restOfStatement = statement.substring(psn + 9).trim();
-		return "What would it mean to " + restOfStatement + "?";
+		String restOfStatement = statement.substring(psn + 7).trim();
+	
+		return "Would you be really happy if you had "+ restOfStatement + "?";
 	}
-
+private String transformIYouStatement(String statement)
+{
+	statement = statement.trim();
+	String lastChar = statement.substring(statement
+			.length() - 1);
+	if (lastChar.equals(".")||lastChar.equals("!"))
+	{
+		statement = statement.substring(0, statement
+				.length() - 1);
+	}
+	
+	int psnOfYou = findKeyword (statement, "I", 0);
+	int psnOfMe = findKeyword (statement, "You", psnOfYou + 1);
+	
+	String restOfStatement = statement.substring(psnOfYou + 1, psnOfMe).trim();
+	return "Why do you " + restOfStatement + " me?";
+}
 	
 	
 	/**
