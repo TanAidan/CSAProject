84a28634f69036e81d2c6873ecc48fb98b10ff6e
Aidan Tan <TanA5915@sduhsd.lan>
2018-03-14 11:11:19 -0700

    gradeBookRunner

--- CSAProject/src/unit11/GradeBookRunner.java
+++ CSAProject/src/unit11/GradeBookRunner.java
@@ -19,7 +19,29 @@ public class GradeBookRunner
 		out.println("Welcome to the Class Stats program!");
 		
 		Scanner keyboard = new Scanner(System.in);
+		System.out.println("\n\n What is the name of this class");
+		String className = keyboard.nextLine();
+		System.out.println("\n\nHow many students are in this class");
 		
+		int numStudents = keyboard.nextInt();
+		keyboard.nextLine();
+		Class c = new Class(className,numStudents);
+		for(int i = 0; i<numStudents;i++)
+		{
+			System.out.print("Enter the name of student"+(i+1)+" : ");
+			String stuName = keyboard.nextLine();
+			System.out.println("Enter the grades for "+stuName);
+			System.out.println("Use the formal (number of grades) - grade grade grade (ex) 2 - 100 56");
+			String gradeList  = keyboard.nextLine();
+			c.addStudent(i, new Student(stuName,gradeList));
+			
+		}
+		System.out.println("\n\n"+c);
+		c.sort();
+		System.out.println("\n\n"+c);
+		System.out.println("Failure List - "+ c.getFailureList(70));
+		System.out.println("Highest Average - "+c.getStudentWithHighestAverage());
+		System.out.println("Lowest Average - "+c.getStudentWithLowestAverage());
 		
 
 
